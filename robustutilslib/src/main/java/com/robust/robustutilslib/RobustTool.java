c0c91575224428b958ee6ddab7ebb833ef81f828
chenhewen <532531210@qq.com>
2017-07-11 11:45:48 +0800

    move context to init method

--- robustutilslib/src/main/java/com/robust/robustutilslib/RobustTool.java
+++ robustutilslib/src/main/java/com/robust/robustutilslib/RobustTool.java
@@ -18,30 +18,33 @@ public class RobustTool {
     public static final int SHOW_TYPE_LOG = 1;
     public static final int SHOW_TYPE_TOAST = 1 << 1;
 
+    private static Context sAppContext;
+
     private static int sType = SHOW_TYPE_LOG;
     private static long sMainThreadId;
 
     private static Handler sHandler = new Handler(Looper.getMainLooper());
 
-    public static void init(String tag, int type) {
+    public static void init(Context appContext, String tag, int type) {
+        sAppContext = appContext;
         sTag = tag;
         sType = type;
         sMainThreadId = Thread.currentThread().getId();
     }
 
-    public static void log(final Context context, final String log) {
+    public static void log(final String log) {
         if ((sType & SHOW_TYPE_LOG) != 0) {
             Log.i(sTag, log);
         }
         if ((sType & SHOW_TYPE_TOAST) != 0) {
             long curThreadId = Thread.currentThread().getId();
             if (curThreadId == sMainThreadId) {
-                Toast.makeText(context, log, Toast.LENGTH_SHORT).show();
+                Toast.makeText(sAppContext, log, Toast.LENGTH_SHORT).show();
             } else {
                 sHandler.post(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(context, log, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(sAppContext, log, Toast.LENGTH_SHORT).show();
                     }
                 });
             }
